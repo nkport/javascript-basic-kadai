2d8e7d9c11adaeb112ab4c1eac00b93dbb09df74
nkport <ktmtworks@outlook.jp>
2023-12-13 09:41:07 +0900

    「非同期処理を使用して、2秒後に文章を書き換えよう」課題提出

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+// HTMLから必要な部分の取得
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+// ボタン押すと文字が切り替わる
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        text.textContent = 'ボタンをクリックしました';
+    }, 2000);
+
+});
